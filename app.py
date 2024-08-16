192e1998296b5840620258218e0d667fd2925b1b
chuck-alt-delete <chuckinator0@gmail.com>
2024-08-16 15:13:03 -0700

    make a table of previous assignments

--- app.py
+++ app.py
@@ -13,12 +13,13 @@ def get_homepage():
             Div(
                 P("hello", style="text-align: center;")
             )
-        )
+        ),
+        A(href='/assignments', title="past assignments")
     )
 
 @app.get("/assignments")
 def get_assignments():
-    rows = db.query(
+    results = db.query(
         """
         select 
             u1.name as gifter, 
@@ -31,12 +32,19 @@ def get_assignments():
         order by year desc
         """
         )
-    return Ul(
-        *[Li(
-            P(f"Gifter: {row['gifter']}"),
-            P(f"Giftee: {row['giftee']}"),
-            P(f"Year: {row['year']}")
-        ) for row in rows]
+    rows = [
+        Tr(
+            *[Td(value) for value in result.values()]
+        )
+        for result in results
+        ]
+    head = Thead(
+        *[Th(column) for column in ("gifter", "giftee", "year")],
+        cls="bg-purple/10")
+    return Table(
+        head,
+        *rows,
+        style='margin-left: auto; margin-right:auto'
     )
 
 serve()
